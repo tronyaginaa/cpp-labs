d00e91485a853fd982883f49f5084958fd2fb557
tronyaginaa <tronyaginaa@gmail.com>
2022-06-16 19:44:01 +0300

    Create main.cpp

---
+++ arcanoid-lab/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  arkanoidNew
+//
+//  Created by Alexandra Tronyagina on 23.05.2022.
+//
+
+#include <iostream>
+#include "Game.hpp"
+
+int main() {
+    Game game;
+    game.Run();
+    return 0;
+}
+
